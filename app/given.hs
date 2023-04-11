95d66d8cd841e12071db37a2cd93528e9dba1cb6
Sebastian Hermansson <hermansson.sebbe@gmail.com>
2023-04-11 14:40:01 +0200

    Completed Assignment 1 on LabA

--- app/given.hs
+++ app/given.hs
@@ -3,12 +3,14 @@ import System.Random
 import Criterion.Main
 import Control.Parallel
 import Control.Parallel.Strategies
+import Control.Monad.Par
 
 -- code borrowed from the Stanford Course 240h (Functional Systems in Haskell)
 -- I suspect it comes from Bryan O'Sullivan, author of Criterion
 
 data T a = T !a !Int
 
+-- A parallel map using par and pseq
 pmap :: (a->b) -> [a] -> [b]
 pmap _ [] = []
 --pmap f (x:xs) = fx `par` fxs `pseq` (fx : fxs)
@@ -17,20 +19,26 @@ pmap f (x:xs) = pseq (par fx fxs) (fx : fxs)
         fx = f x
         fxs = pmap f xs
 
---rmap :: (a->b) -> [a] -> [b]
---rmap _ [] = []
---rmap f (x:xs) = runEval $ do 
-
-pMap :: (a -> b) -> [a] -> [b]
-pMap f [] = []
-pMap f (a:as) = runEval $ do
+-- A parallel map using rpar
+rmap :: (a -> b) -> [a] -> Eval [b]
+rmap _ [] = return []
+rmap f (a:as) = do
   b <- rpar (f a)
-  bs <- pMap f as
+  bs <- rmap f as
   return (b:bs)
 
+-- A parallel map using strategies
+smap :: NFData b => (a -> b) -> [a] -> [b]
+smap f xs = map f xs `using` parList rdeepseq
+
+-- A parallel map using par monad
+pmmap :: NFData b => (a -> Par b) -> [a] -> Par [b]
+pmmap f as = do
+  ibs <- mapM (spawn . f) as
+  mapM get ibs
 
 mean :: (RealFrac a) => [a] -> a
-mean = fini . foldl' go (T 0 0)
+mean = do fini . foldl' go (T 0 0) 
   where
     fini (T a _) = a
     go (T m n) x = T m' n'
@@ -43,20 +51,28 @@ resamples k xs =
     zipWith (++) (inits xs) (map (drop k) (tails xs))
 
 -- Regular jackknife
---jackknife :: ([a] -> b) -> [a] -> [b]
---jackknife f = map f . resamples 500
+jackknife :: ([a] -> b) -> [a] -> [b]
+jackknife f = map f . resamples 500
 
 -- Jackknife using pmap (par and pseq implementation)
---jackknife :: ([a] -> b) -> [a] -> [b]
---jackknife f = pmap f . resamples 500
+pJackknife :: ([a] -> b) -> [a] -> [b]
+pJackknife f = pmap f . resamples 500
 
 -- Jackknife using Eval monad
-jackknife :: ([a] -> b) -> [a] -> [b]
-jackknife f = pMap f . resamples 500
+rJackknife :: ([a] -> b) -> [a] -> Eval [b]
+rJackknife f = rmap f . resamples 500
 
--- Jackknife using parMap (built in)
---jackknife :: NFData b => ([a] -> b) -> [a] -> [b]
---jackknife f = parMap rseq f . resamples 500
+-- Jackknife using parMap (built in). Can not be run with par monad imported
+--parJackknife :: NFData b => ([a] -> b) -> [a] -> [b]
+--parJackknife f xs = parMap rdeepseq f (resamples 500 xs)
+
+-- Jackknife using smap (strategies implementation)
+sJackknife :: NFData b => ([a] -> b) -> [a] -> [b]
+sJackknife f xs = smap f (resamples 500 xs)
+
+-- Jackknife using pmmap (par monad implementation)
+pmJackknife :: NFData b => ([a] -> Par b) -> [a] -> Par [b]
+pmJackknife f xs = pmmap f (resamples 500 xs)
 
 
 crud = zipWith (\x a -> sin (x / 300)**2 + a) [0..]
@@ -73,6 +89,10 @@ main = do
   putStrLn $ "jack mean min:  " ++ show (minimum j)
   putStrLn $ "jack mean max:  " ++ show (maximum j)
   defaultMain
-        [
-         bench "jackknife" (nf (jackknife  mean) rs)
+        [ bench "jackknife" (nf (jackknife  mean) rs)
+        , bench "pJackknife" (nf (pJackknife  mean) rs)
+        , bench "rJackknife" (nf (runEval . rJackknife  mean) rs)
+        --, bench "parJackknife" (nf (parJackknife mean) rs)
+        , bench "sJackknife" (nf (sJackknife mean) rs)
+        , bench "pmJackknife" (nf (runPar . pmJackknife (return . mean)) rs)
          ]
