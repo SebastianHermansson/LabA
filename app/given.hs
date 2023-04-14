f4dc47109e157ce08d8d88ad7cab4f12a75085bf
Sebastian Hermansson <hermansson.sebbe@gmail.com>
2023-04-14 13:58:58 +0200

    h

--- app/given.hs
+++ app/given.hs
@@ -27,7 +27,7 @@ rmap f (a:as) = do
   bs <- rmap f as
   return (b:bs)
 
--- A parallel map using strategies
+-- A parallel map using parList strategy
 smap :: NFData b => (a -> b) -> [a] -> [b]
 smap f xs = map f xs `using` parList rdeepseq
 
@@ -36,6 +36,71 @@ pmmap :: NFData b => (a -> Par b) -> [a] -> Par [b]
 pmmap f as = do
   ibs <- mapM (spawn . f) as
   mapM get ibs
+
+----------------------------------------------------------
+-- Merge sort using parallelization
+
+-- A merge sort algorithm
+mergeSort :: Ord a => [a] -> [a]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs =
+    let (left, right) = split' xs
+    in merge (mergeSort left) (mergeSort right)
+
+-- A function that splits a list on the middle
+split' :: [a] -> ([a], [a])
+split' xs = splitAt (length xs `div` 2) xs
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+    | x < y     = x : merge xs (y:ys)
+    | otherwise = y : merge (x:xs) ys
+
+-- A higher order parallel divide and conquer function
+divideAndConquer :: ([a] -> Bool) -> ([a] -> b) -> ((b,b) -> b) -> ([a] -> ([a],[a])) -> [a] -> Par b
+divideAndConquer baseCase solve combine divide xs =
+    if baseCase xs
+        then return $ solve xs
+        else do
+            let (left, right) = divide xs
+            leftResult  <- spawn $ divideAndConquer baseCase solve combine divide left
+            rightResult <- spawn $ divideAndConquer baseCase solve combine divide right
+            leftVal  <- get leftResult
+            rightVal <- get rightResult
+            return $ combine (leftVal, rightVal)
+
+
+-- A parallel merge sort algorithm utilizing our higher order function
+pMergeSort :: (Ord a) => [a] -> [a]
+pMergeSort = runPar . divideAndConquer baseCase solve combine divide
+  where
+   baseCase [x] = True
+   baseCase _ = False
+   solve [x] = [x]
+   combine (a,b) = merge a b
+   divide = split'
+
+---------------------------------------------------------------
+-- Sudoku solverprep and map functions with strategies
+
+import Sudoku
+import Control.Exception
+import System.Environment
+import Data.Maybe
+
+someStratMap :: NFData b => (a -> b) -> [a] -> [b]
+someStratMap f xs = map f xs `using` parBuffer rdeepseq
+
+--someStratMap :: NFData b => (a -> b) -> [a] -> [b]
+--someStratMap f xs = map f xs `using` parList rdeepseq
+
+--someStratMap :: NFData b => (a -> b) -> [a] -> [b]
+--someStratMap f xs = map f xs `using` parListChunk rdeepseq
+
+-------------------------------------------------------------------------
 
 mean :: (RealFrac a) => [a] -> a
 mean = do fini . foldl' go (T 0 0) 
@@ -68,11 +133,11 @@ rJackknife f = rmap f . resamples 500
 
 -- Jackknife using smap (strategies implementation)
 sJackknife :: NFData b => ([a] -> b) -> [a] -> [b]
-sJackknife f xs = smap f (resamples 500 xs)
+sJackknife f = smap f . resamples 500
 
 -- Jackknife using pmmap (par monad implementation)
 pmJackknife :: NFData b => ([a] -> Par b) -> [a] -> Par [b]
-pmJackknife f xs = pmmap f (resamples 500 xs)
+pmJackknife f = pmmap f . resamples 500
 
 
 crud = zipWith (\x a -> sin (x / 300)**2 + a) [0..]
@@ -88,11 +153,24 @@ main = do
   let j = jackknife mean rs :: [Float]
   putStrLn $ "jack mean min:  " ++ show (minimum j)
   putStrLn $ "jack mean max:  " ++ show (maximum j)
+
+  -- Merge sort test
+  putStrLn $ "Mergesort output:  " ++ show (pMergeSort [3,1,6])
+
+  -- Sudoku thing
+  file <- readFile "./app/sudoku.txt"
+
+  let puzzles   = lines file
+      solutions = runEval (someStratMap solve puzzles)
+
+  evaluate (length puzzles)
+  print (length (filter isJust solutions))
+
   defaultMain
         [ bench "jackknife" (nf (jackknife  mean) rs)
         , bench "pJackknife" (nf (pJackknife  mean) rs)
         , bench "rJackknife" (nf (runEval . rJackknife  mean) rs)
-        --, bench "parJackknife" (nf (parJackknife mean) rs)
+        , bench "parJackknife" (nf (parJackknife mean) rs)
         , bench "sJackknife" (nf (sJackknife mean) rs)
         , bench "pmJackknife" (nf (runPar . pmJackknife (return . mean)) rs)
          ]
